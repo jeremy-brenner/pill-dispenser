076a9a9e946bff9d075f1160634e4333fa5a798e
Jeremy J Brenner <jeremyjbrenner@gmail.com>
2022-06-17 14:22:01 -0500

    add fanciness to status script and tweak lockfordays

--- getStatus.sh
+++ getStatus.sh
@@ -13,7 +13,16 @@ statusField () {
   echo ${STATUS} | jq -r ".${1}"
 }
 
+dateStamp () {
+  echo `date --date=@${1}`
+}
+
 STATUS=`curl -s pillbox/status`
+
+if [ ! $STATUS ] ; then
+  echo "ERROR"
+  exit
+fi
 
 IS_LOCKED=$(statusField 'isLocked')
 
@@ -30,20 +39,46 @@ DEBUG=$(statusField 'debug')
 
 UNLOCK_TIME=$(statusField 'unlockTime')
 CURRENT_TIME=$(statusField 'currentTime')
+LOCAL_TIME=`date +%s`
+
+READY_DATESTAMP=$(dateStamp $READY_TIME)
+CURRENT_DATESTAMP=$(dateStamp $CURRENT_TIME) 
+UNLOCK_DATESTAMP=$(dateStamp $UNLOCK_TIME)
 
 SECONDS_TO_UNLOCK=$(( ${UNLOCK_TIME} - ${CURRENT_TIME} ))
 if [ "${SECONDS_TO_UNLOCK}" -lt 0 ]; then
   SECONDS_TO_UNLOCK=0
 fi
 
+
 MINUTES_TO_UNLOCK=$(( ${SECONDS_TO_UNLOCK} / 60 ))
 TIME_TO_UNLOCK="$(prettyTime $SECONDS_TO_UNLOCK)"
 MININUM_SECONDS_TO_UNLOCK=$(( ${MINIMUM_UNLOCK_TIME} * 60 ))
+MINIMUM_TIME_TO_UNLOCK="$(prettyTime $MININUM_SECONDS_TO_UNLOCK)"
+
+MINIMUM_UNLOCK_TIMESTAMP=$(( ${MININUM_SECONDS_TO_UNLOCK} + ${CURRENT_TIME} ))
+MINIMUM_UNLOCK_DATESTAMP=$(dateStamp $MINIMUM_UNLOCK_TIMESTAMP)
+
+TIME_DIFF=$(( ${CURRENT_TIME} - ${LOCAL_TIME} ))
+ALLOWED_DIFF=5
+
+PFS="%-20s %-31s %10s %-20s\n"
 
 if [ "${DEBUG}" -eq 1 ]; then
   echo "*** DEBUG IS ON ***"
 fi
 
-echo "State: ${STATE}"
-echo "Time to unlock: ${MINUTES_TO_UNLOCK}m (${TIME_TO_UNLOCK})"
-echo "Minimum Unlock Time: ${MINIMUM_UNLOCK_TIME}m ($(prettyTime $MININUM_SECONDS_TO_UNLOCK))"
+if [ ${TIME_DIFF} -gt ${ALLOWED_DIFF} -o ${TIME_DIFF} -lt -${ALLOWED_DIFF} ]; then
+  echo "TIME MORE THAN ${ALLOWED_DIFF} SECONDS OFF!!!"
+  printf "${PFS}" "LOCAL TIME:" "${LOCAL_TIME}"
+  printf "${PFS}" "SYSTEM TIME:" "${CURRENT_TIME}"
+  printf "${PFS}" "DIFFERENCE:" "${TIME_DIFF}"
+fi
+
+printf "${PFS}" "State:" "${STATE}"
+printf "${PFS}" "Ready time:" "${READY_DATESTAMP}"
+printf "${PFS}" "Sytem time:" "${CURRENT_DATESTAMP}"
+printf "${PFS}" "Time to unlock:" "${UNLOCK_DATESTAMP}" "${MINUTES_TO_UNLOCK}m" "(${TIME_TO_UNLOCK})"
+printf "${PFS}" "Minimum Unlock Time:" "${MINIMUM_UNLOCK_DATESTAMP}" "${MINIMUM_UNLOCK_TIME}m" "(${MINIMUM_TIME_TO_UNLOCK})"
+
+
--- lockForDays.sh
+++ lockForDays.sh
@@ -3,6 +3,7 @@
 ## Open on the morning of the nth day.
 
 OPEN_HOUR=8
+MINIMUM=1440
 
 MIN=`date +%M`
 HOUR=`date +%k`
@@ -12,7 +13,10 @@ MINUTES_TO_OPEN_HOUR=$(( OPEN_HOUR*60 + MINUTES_TO_MIDNIGHT ))
 FULL_DAYS=$(( $1 - 1 ))
 FULL_DAYS_IN_MINUTES=$(( FULL_DAYS*24*60 ))
 MINUTES_TO_OPEN=$(( FULL_DAYS_IN_MINUTES + MINUTES_TO_OPEN_HOUR ))
-
+if [ ${MINUTES_TO_OPEN} -lt ${MINIMUM} ] ; then
+	echo "${MINUTES_TO_OPEN} is less than minimum, setting to ${MINIMUM}"
+	MINUTES_TO_OPEN=1440;
+fi
 echo Minutes to open: ${MINUTES_TO_OPEN}
 
 curl pillbox/lock
